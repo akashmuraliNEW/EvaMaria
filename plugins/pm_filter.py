9d1d9eaf500cc4f4599819d20966ca4dec3aaa13
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-06-04 15:57:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -783,7 +783,6 @@ async def advantage_spell_chok(msg):
     a = await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(15)
-    await message.delete()
     await a.delete()
 
 
