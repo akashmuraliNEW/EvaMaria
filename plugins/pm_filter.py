b0e312e36adbecce6523020137b2a5b373ff3522
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-06-05 09:10:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -35,7 +35,7 @@ async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
-        await asyncio.sleep(10)
+        await asyncio.sleep(1800)
         await client.delete_messages(message.chat.id, message.message_id)
 
 @Client.on_callback_query(filters.regex(r"^next"))
